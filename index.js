ccb1e375abd8c5f1c9a0a9e333f1488040e7757c
Zach <shaw.zacht@gmail.com>
2022-02-22 22:17:26 -0800

    commit tree d8863f14604854ed07809bec3b5512d656cbf969

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const headquarters = 42;
+const oneBlock = 264;
+
+function distanceFromHqInBlocks(start) {
+    return Math.abs(headquarters - start);
+}
+
+function distanceFromHqInFeet(start) {
+   let blocks = distanceFromHqInBlocks(start);
+   return blocks * oneBlock;
+}
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs(start - destination) * oneBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTravelled = distanceTravelledInFeet(start, destination);
+    if (feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled >400 && feetTravelled <= 2000) {
+        return (feetTravelled-400) * .02;
+    } else if (feetTravelled >2000 && feetTravelled<=2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
